0ff0ce5cd657ffa4c605e9497580b1af655c29d2
Farouk568-f <78809939+Farouk568-f@users.noreply.github.com>
2025-10-01 23:39:07 +0100

    Update constants.ts

--- contexts/constants.ts
+++ contexts/constants.ts
@@ -7,7 +7,7 @@ export const BACKDROP_SIZE = "w1280";
 export const BACKDROP_SIZE_MEDIUM = "w780";
 
 // IMPORTANT: This is the new backend URL for fetching stream links.
-export const SCRAPER_API_URL = "https://2338eb27d80f.ngrok-free.app /scrape";
+export const SCRAPER_API_URL = "https://be57c84e6a36.ngrok-free.app/scrape";
 
 export const AVAILABLE_PROVIDERS = [
     { id: 'moviebox', name: 'MovieBox' },
